b127e2d51b81c25cca74466a1b347d01070fccd4
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2025-01-06 11:46:28 +0000

    ATO-1310: add method to take value options (as well as toggle options)

--- acceptance-tests/src/test/java/uk/gov/di/test/step_definitions/RpStubStepDef.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/step_definitions/RpStubStepDef.java
@@ -9,9 +9,17 @@ public class RpStubStepDef extends BasePage {
     RpStubPage rpStubPage = new RpStubPage();
 
     @When("the user comes from the stub relying party with options: {string}")
-    public void theExistingUserVisitsTheStubRelyingParty(String options) {
+    public void theExistingUserVisitsTheStubRelyingParty(String toggleOptions) {
         rpStubPage.goToRpStub();
-        rpStubPage.selectRpOptionsByIdAndContinue(options);
+        rpStubPage.selectRpOptionsByIdAndContinue(toggleOptions, "default");
+        setAnalyticsCookieTo(false);
+    }
+
+    @When("the user comes from the stub relying party with options: {string} and {string}")
+    public void theExistingUserVisitsTheStubRelyingPartyAndSetsValueOptions(
+            String toggleOptions, String valueOptions) {
+        rpStubPage.goToRpStub();
+        rpStubPage.selectRpOptionsByIdAndContinue(toggleOptions, valueOptions);
         setAnalyticsCookieTo(false);
     }
 }
